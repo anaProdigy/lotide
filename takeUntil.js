241840730f9b1e95baec62031dc713c32b63ec9e
Anastasia <z.anastasia11@gmail.com>
2023-02-10 17:16:45 -0800

    find a key in object function

--- takeUntil.js
+++ takeUntil.js
@@ -40,7 +40,6 @@ const eqArrays = function(array1, array2) {
 
 
 
-
 const assertArraysEqual = function(array1, array2) {
   if (!eqArrays(array1, array2)) {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);

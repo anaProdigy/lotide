752168c5664dd528b5f74268a5797ab9b636146f
Anastasia <z.anastasia11@gmail.com>
2023-02-13 16:23:04 -0800

    refactor eqArray function implementing module.export

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 //a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
@@ -23,7 +17,4 @@ const eqArrays = function (array1, array2) {
 
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
+module.exports = eqArrays;

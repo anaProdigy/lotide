325bfb7d789978463e6c75e937508875d2eec9e0
Anastasia <z.anastasia11@gmail.com>
2023-02-13 16:27:48 -0800

    refactor assertArraysEqual function implementing module.export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,17 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
 
 //Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. A function which utilizes eqArrays.js to compare two arrays for an exact match, then prints a pass or fail assertion
 
 
-const assertArraysEqual = function (array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   if (!eqArrays(array1, array2)) {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   } else {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   }
-  }
+};
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
+
+
+
+module.exports = assertArraysEqual;

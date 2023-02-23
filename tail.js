5643b8ba203a0ad0798a9532f6934b7c642905fd
Anastasia <z.anastasia11@gmail.com>
2023-02-23 14:40:58 -0800

    add tests to the finction tail

--- tail.js
+++ tail.js
@@ -1,15 +1,27 @@
 const assertEqual = require('./assertEqual');
 
 
-const tail = function(array) {
-  let resultTail = [];
-  for (let i=1; i < array.length; i++) {
-    resultTail.push(array[i])
+let newArray = [];
+const tail = (array) => {
+
+  for (i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
   }
-  return resultTail;
-}
+  return newArray;
 
+};
 
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
 module.exports = tail;

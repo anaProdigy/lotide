45ddcf144350d57732cbcd80825d8c418daf01f2
Anastasia <z.anastasia11@gmail.com>
2023-02-23 09:18:45 -0800

    add tests to assertEqual and refactor the code

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,5 @@
 // FUNCTION IMPLEMENTATION
+// The function compares the two values it takes in and print out a message telling us if they match or not.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,10 +7,11 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-
-
-
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 3);
 
 
 module.exports = assertEqual;

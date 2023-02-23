caf09b9a652eece04b8d519de282492d68e64424
Anastasia <z.anastasia11@gmail.com>
2023-02-23 14:26:11 -0800

    add more tests to head function

--- head.js
+++ head.js
@@ -1,8 +1,15 @@
-const assertEqual = require('./assertEqual');
+ const assertEqual = require('./assertEqual');
 
 
-const head = function(array) {
+ const head = function(array) {
   return array[0];
 }
 
-module.exports = head;
+
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);
+
+ module.exports = head;

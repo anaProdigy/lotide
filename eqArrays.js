193141918200124a1d52d26fe8796eef464dbaa2
Anastasia <z.anastasia11@gmail.com>
2023-02-24 10:48:58 -0800

    add more tests to test esge cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,35 @@
 const assertEqual = require('./assertEqual');
 
-
-//a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
+const eqArrays = (arr1, arr2) => {
+  //check id arrays are the same length
+  if (arr1.length !== arr2.length) {
     return false;
   }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] !== arr2[i]) {
       return false;
+
     }
+   
   }
   return true;
-}
+};
 
+
+
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays([1, 2, 3], [3, 2]), false)
 
 
 module.exports = eqArrays;

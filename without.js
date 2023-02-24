07c6402401922dd9cd98675fb8d1de1a447eb981
Anastasia <z.anastasia11@gmail.com>
2023-02-24 11:31:35 -0800

    refactor thecode using .includes

--- without.js
+++ without.js
@@ -29,27 +29,30 @@ const assertArraysEqual = function(array1, array2) {
 
 //Implement without which will return a subset of a given array, removing unwanted elements.This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
 
+const without = (source, itemsToRemove) => {
+  const newArray = [];
+for ( let i = 0; i < source.length; i++) { 
+  
+  if (!itemsToRemove.includes( source[i])) { 
+   newArray.push(source[i])
+}
+}
 
-
-
-const without = function(source, itemsToRemove) {
-  let newArray = [];
-  for (let element of source) {
-    if (!itemsToRemove.includes(element)) {
-      newArray.push(element);
-    }
-
-  }
   return newArray;
 }
 
-without([1, 2, 3], [1]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+
+
+//console.log(without([1, 2, 3], [1])); // => [2, 3]
+//console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 
 //Test case:The without function should be returning a new array and not modify the original array that is passed in. Let's write a test case to ensure this:
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+//  console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+//  assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+// assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"])

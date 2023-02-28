9dfe7e52cb05594fe6ea0ab7a6c0bb0c0ebcb353
Anastasia <z.anastasia11@gmail.com>
2023-02-27 16:17:48 -0800

    cleaa up count only

--- countOnly.js
+++ countOnly.js
@@ -7,36 +7,28 @@ const assertEqual = function(actual, expected) {
 };
 
 
-
-// This function should take in a collection of items and return counts for a specific subset of those items. It won't count everything. In order to decide what to count, it will also be given an idea of which items we care about and it will only count those, ignoring the others.
-
-//Items in our case will be limited to Strings.
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-
     if(itemsToCount[item]) {
-    // inside the loop,
-    // increment the counter for each item:
-    //   set a property with that string key to:
-    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    
     if (results[item]) {
       results[item] += 1;
     } else {
       results[item] = 1;
     }
+    
     }
-
-    //console.log(item);
+   
   }
-
+  
   return results;
 
 };
+
 
 
 
@@ -61,3 +53,36 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+
+
+
+// This function should take in a collection of items and return counts for a specific subset of those items. It won't count everything. In order to decide what to count, it will also be given an idea of which items we care about and it will only count those, ignoring the others.
+
+//Items in our case will be limited to Strings.
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+// const countOnly = function(allItems, itemsToCount) {
+//   const results = {};
+
+//   for (const item of allItems) {
+
+//     if(itemsToCount[item]) {
+//     // inside the loop,
+//     // increment the counter for each item:
+//     //   set a property with that string key to:
+//     //     the value that was already there (or zero if nothing there) with 1 added to it.
+//     if (results[item]) {
+//       results[item] += 1;
+//     } else {
+//       results[item] = 1;
+//     }
+//     }
+
+//     //console.log(item);
+//   }
+
+//   return results;
+
+// };
--- tail.js
+++ tail.js
@@ -11,8 +11,8 @@ const tail = (array) => {
 
 };
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+ assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
 // assertEqual(result.length, 2); // ensure we get back two elements
 // assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"

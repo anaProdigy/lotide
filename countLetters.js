ac869a518054457686e8e157d2676ac3fee75b8f
Anastasia <z.anastasia11@gmail.com>
2023-02-27 16:47:02 -0800

    delete comments

--- countLetters.js
+++ countLetters.js
@@ -8,19 +8,68 @@ const assertEqual = function(actual, expected) {
 
 
 
-const countLetters = function(sentence) {
-  let results = {};
+const countLetters = (str) => {
+  let result = {};
+  const separated = str.split("");
 
-  for (let letter of sentence) {
+  for (let letter of separated) {
     if (letter !== " ") {
-      if (results[letter]) {
-        results[letter] += 1;
+      if (result[letter]) {
+        
+        result[letter] += 1;
       } else {
-        results[letter] = 1;
+        result[letter] = 1;
       }
     }
   }
-  return results;
-};
 
+  console.log("result", result);
+
+  return result;
+
+};
 console.log(countLetters("lighthouse in the house"));
+
+//countLetters("lighthouse in the house");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const countLetters = function(sentence) {
+//   let results = {};
+
+//   for (let letter of sentence) {
+//     if (letter !== " ") {
+//       if (results[letter]) {
+//         results[letter] += 1;
+//       } else {
+//         results[letter] = 1;
+//       }
+//     }
+//   }
+//   return results;
+// };
+
+

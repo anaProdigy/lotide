dd591f394d6e373d5e3e4828ce1de3a2e66377c5
Anastasia <z.anastasia11@gmail.com>
2023-03-01 16:20:51 -0800

    add modified fn

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -29,27 +29,28 @@ const assertArraysEqual = function(array1, array2) {
   } else {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   }
-}
+};
 
 
 
 const eqObjects = function(object1, object2) {
+  const arr1 = Object.keys(object1);
+  const arr2 = Object.keys(object2);
 
-  let array1 = Object.keys(object1);
-  let array2 = Object.keys(object2);
-  // console.log("array1: ", array1)
-  //   console.log("array2: ", array2)
-  if (array1.length !== array2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let key of array1) {
+
+  for (const key in object1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key]))
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
+      } else continue;
     }
-
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
   }
-
   return true;
 };
 
@@ -58,15 +59,51 @@ const eqObjects = function(object1, object2) {
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  console.log(`Example label: ${inspect(actual)}`);
-
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
   }
+  
 };
 
 const ab = { a: "5", b: "2" };
 const ba = { b: "2", a: "5" };
 assertObjectsEqual(ab, ba);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const assertObjectsEqual = function(actual, expected) {
+//   const inspect = require('util').inspect;
+//   console.log(`Example label: ${inspect(actual)}`);
+
+//   if (eqObjects(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+//   }
+// };

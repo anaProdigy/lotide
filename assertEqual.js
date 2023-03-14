df975c169c7f5a13c74db43185b3dc2e7ce8f9bb
Anastasia <z.anastasia11@gmail.com>
2023-03-14 15:04:06 -0700

    change test code

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual("Bootcamp", "Bootcamp");
 // assertEqual(1, 1);
 // assertEqual(1, 3);
--- head.js
+++ head.js
@@ -7,9 +7,9 @@
 
 
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), 5);
 
  module.exports = head;

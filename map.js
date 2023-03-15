0221d0a571d1a45687f396f1985aba6ea14fd87a
Anastasia <z.anastasia11@gmail.com>
2023-03-14 21:31:58 -0700

    reconfigure map function and tests

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 //Our map function will take in two arguments:
 
 // An array to map
@@ -18,35 +20,6 @@ const map = (array, callback) => {
 const result1 = map(words, word => word[0])
 console.log(result1);
 
-
-
-
-
-// // FUNSTIONS FOR TESTING
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. A function which utilizes eqArrays.js to compare two arrays for an exact match, then prints a pass or fail assertion
-
-
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  }
-};
 
 
 // // TEST 1

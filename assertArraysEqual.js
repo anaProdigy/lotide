8ad8bb05d6986e4084ec4d1dbf9bd66d06bbbc5f
Anastasia <z.anastasia11@gmail.com>
2023-03-14 21:51:11 -0700

    add all functions to index file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,8 +12,8 @@ const assertArraysEqual = (array1, array2) => {
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
 
 
 
--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetter = require('./countLetter');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const flatten = require('./flatten');
+const flatten = require('./flatten');
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetter,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  middle,
+  takeUntil,
+  without
 };

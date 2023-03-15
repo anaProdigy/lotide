bfb4da47bc4c0bedc0224ea24aa83cfcfa85595a
Anastasia <z.anastasia11@gmail.com>
2023-03-14 21:20:11 -0700

    reconfigure eqArrays function and tests

--- eqArrays.js
+++ eqArrays.js
@@ -20,16 +20,6 @@ const eqArrays = (arr1, arr2) => {
 
 
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
-// assertEqual(eqArrays([1, 2, 3], [3, 2]), false)
 
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,39 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const eqArrays = require('../eqArrays');
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
+
+
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
+describe("#eqArrays", () => {
+  it("returns true if both arrays have identical value ", () => {
+    const expectedResult = true;
+    const actualResult = eqArrays([1, 2, 3], [1, 2, 3]);
+    assert.strictEqual(actualResult, expectedResult);
+  });
+
+
+  it("returns false if both arrays have identical length but different order values ", () => {
+    const expectedResult = false;
+    const actualResult = eqArrays([1, 2, 3], [3, 2, 1]);
+    assert.strictEqual(actualResult, expectedResult);
+  });
+
+
+  it("returns true if both arrays have identical length same values as strings ", () => {
+    const expectedResult = true;
+    const actualResult = eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+    assert.strictEqual(actualResult, expectedResult);
+  });
+
+  it("returns false if both arrays have identical length but values are mixed - nums and strings", () => {
+    const expectedResult = false;
+    const actualResult = eqArrays(["1", "2", "3"], ["1", "2", 3]);
+    assert.strictEqual(actualResult, expectedResult);
+  });
+});

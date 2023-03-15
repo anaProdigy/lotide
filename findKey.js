a14c8f741c27c90beee13e73193478aa4c7c9e31
Anastasia <z.anastasia11@gmail.com>
2023-03-14 21:29:30 -0700

    reconfigure findKey function and tests

--- findKey.js
+++ findKey.js
@@ -1,3 +1,4 @@
+const assertEqual = require('./assertEqual');
 // Implement the function findKey which takes in an object and a callback.It should scan the object and return the first key for which the callback returns a truthy value.If no key is found, then it should return undefined.
 const findKey = (obj, cb) => {
   for (const key in obj) {
@@ -11,21 +12,6 @@ const findKey = (obj, cb) => {
 
 
 
-
-
-
-
-
-
-
-// FUNSTIONS FOR TESTING
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 // TEST CASE 1
 const result = findKey({

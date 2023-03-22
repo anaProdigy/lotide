4ffbd1fa961c1ab7099cbfa3a8ec7a155950790d
Anastasia <z.anastasia11@gmail.com>
2023-03-22 11:00:36 -0700

    create takeUntislTest and move test code from takeUntil there

--- takeUntil.js
+++ takeUntil.js
@@ -16,20 +16,4 @@ const takeUntil = (array, callback) => {
 
 
 
-// TEST CASE 1
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-const expectedResults1 = [1, 2, 5, 7, 2];
-assertArraysEqual(results1, expectedResults1);
-
-// //TEST CASE 2
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-const expectedResults2 = ['I\'ve', 'been', 'to', 'Hollywood'];
-assertArraysEqual(results2, expectedResults2);
-
-// //TEST CASE 3
-const data3 = [];
-const results3 = takeUntil(data3, x => x === ',');
-const expectedResults3 = [];
-assertArraysEqual(results3, expectedResults3);
+module.exports = takeUntil;
